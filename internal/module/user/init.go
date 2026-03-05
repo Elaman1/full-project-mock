b2e6023affab9667c0a0cab18851511f9d9d4db0
Elaman <kaljanelaman39@gmail.com>
2026-03-05 14:15:19 +0500

    fix

--- internal/module/user/init.go
+++ internal/module/user/init.go
@@ -1,37 +1,19 @@
 package user
 
 import (
-	"context"
 	"database/sql"
-	"github.com/Elaman1/full-project-mock/internal/auditlogger"
 	"github.com/Elaman1/full-project-mock/internal/cache"
 	"github.com/Elaman1/full-project-mock/internal/domain/usecase"
-	"github.com/Elaman1/full-project-mock/internal/metrics"
 	"github.com/redis/go-redis/v9"
 )
 
-func InitUserModule(appCtx context.Context, userApp UserApp) *UserHandler {
-	sessionCache := cache.NewSessionRedisRepository(userApp.RedisDB)
-	userRepo := NewUserRepository(userApp.DB)
-
-	// Не готово еще
-	authLogger := auditlogger.InitAuditLogger(appCtx)
-
-	userUsecase := NewUserUsecase(userRepo, userApp.TokenService, sessionCache, authLogger)
-	// Передаем сверху метрику так как не обязательно внутр засовывать
-	return NewUserHandler(userUsecase, userApp.MetricCollector)
+func InitUserModule(db *sql.DB, redisDB *redis.Client, tokenService usecase.TokenService) *UserHandler {
+	sessionCache := cache.NewSessionRedisRepository(redisDB)
+	userRepo := NewUserRepository(db)
+	userUsecase := NewUserUsecase(userRepo, tokenService, sessionCache)
+	return NewUserHandler(userUsecase)
 }
 
-type UserApp struct {
-	DB              *sql.DB
-	RedisDB         *redis.Client
-	TokenService    usecase.TokenService
-	MetricCollector metrics.MetricsCollector
-}
-
-func NewUserHandler(usecase usecase.UserUsecase, metricsCollector metrics.MetricsCollector) *UserHandler {
-	return &UserHandler{
-		Usecase:         usecase,
-		MetricCollector: metricsCollector,
-	}
+func NewUserHandler(usecase usecase.UserUsecase) *UserHandler {
+	return &UserHandler{Usecase: usecase}
 }
